a1f0829584328da55612c5deb610252fd06153f7
steve <1301637976@qq.com>
2023-12-06 11:13:45 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	deleted:    .DS_Store
    	deleted:    .gitignore
    	deleted:    INSTALL.md
    	deleted:    LICENSE
    	deleted:    README.md
    	deleted:    dataset/.DS_Store
    	deleted:    dataset/dataloader.py
    	deleted:    dataset/dataloader_tumor.py
    	deleted:    dataset/dataloader_validation.py
    	deleted:    dataset/dataset_list/.DS_Store
    	deleted:    dataset/dataset_list/.ipynb_checkpoints/split_dataset-checkpoint.ipynb
    	deleted:    dataset/dataset_list/01_Multi-Atlas_Labeling_test.txt
    	deleted:    dataset/dataset_list/01_Multi-Atlas_Labeling_train.txt
    	deleted:    dataset/dataset_list/01_Multi-Atlas_Labeling_train_unlabeled.txt
    	deleted:    dataset/dataset_list/01_Multi-Atlas_Labeling_valid.txt
    	deleted:    dataset/dataset_list/PAOT.txt
    	deleted:    dataset/dataset_list/PAOT_01.txt
    	deleted:    dataset/dataset_list/PAOT_02.txt
    	deleted:    dataset/dataset_list/PAOT_03.txt
    	deleted:    dataset/dataset_list/PAOT_04.txt
    	deleted:    dataset/dataset_list/PAOT_05.txt
    	deleted:    dataset/dataset_list/PAOT_07.txt
    	deleted:    dataset/dataset_list/PAOT_08.txt
    	deleted:    dataset/dataset_list/PAOT_09.txt
    	deleted:    dataset/dataset_list/PAOT_10.txt
    	deleted:    dataset/dataset_list/PAOT_12.txt
    	deleted:    dataset/dataset_list/PAOT_13.txt
    	deleted:    dataset/dataset_list/PAOT_14.txt
    	deleted:    dataset/dataset_list/PAOT_our_train.txt
    	deleted:    dataset/dataset_list/PAOT_test.txt
    	deleted:    dataset/dataset_list/PAOT_train.txt
    	deleted:    dataset/dataset_list/PAOT_val.txt
    	deleted:    dataset/utils.py
    	deleted:    document/.DS_Store
    	deleted:    document/image.png
    	deleted:    document/info.md
    	deleted:    model/.DS_Store
    	deleted:    model/DiNTS.py
    	deleted:    model/Summary_SwinUneTr_Partial.txt
    	deleted:    model/SwinUNETR.py
    	deleted:    model/SwinUNETR_partial.py
    	deleted:    model/Unet.py
    	deleted:    model/Universal_model.py
    	deleted:    optimizers/.DS_Store
    	deleted:    optimizers/lr_scheduler.py
    	deleted:    pretrained_weights/pretrained_checkpoints/.DS_Store
    	deleted:    pretrained_weights/pretrained_checkpoints/README.md
    	deleted:    repo/calculate_attention.py
    	deleted:    repo/create_attention.py
    	deleted:    repo/create_dataset.py
    	deleted:    repo/generate_datalist.py
    	deleted:    repo/generate_datalist.sh
    	deleted:    requirements.txt
    	deleted:    train_semi.py
    	deleted:    train_semi.sh
    	deleted:    utils/.DS_Store
    	deleted:    utils/assemble.py
    	deleted:    utils/calHausdoffDist.py
    	deleted:    utils/cal_volume.py
    	deleted:    utils/check_completeness.py
    	deleted:    utils/compute_dice.py
    	deleted:    utils/displayCTImage.py
    	deleted:    utils/label_transfer.py
    	deleted:    utils/loss.py
    	deleted:    utils/readMetaInfo.py
    	deleted:    utils/regularization.py
    	deleted:    utils/utils.py
    	deleted:    validation.py
    	deleted:    validation.sh
    
    Untracked files:
    	.DS_Store
    	.gitignore
    	INSTALL.md
    	LICENSE
    	README.md
    	dataset/
    	document/
    	model/
    	optimizers/
    	pretrained_weights/
    	repo/
    	requirements.txt
    	train_semi.py
    	train_semi.sh
    	utils/
    	validation.py
    	validation.sh

--- pretrained_weights/pretrained_checkpoints/README.md
+++
@@ -1,2 +0,0 @@
-- Download checkpoint: 
-wget https://www.dropbox.com/s/6ggd0gq5qddahwh/epoch_450.pth
--- repo/generate_datalist.sh
+++
@@ -1,4 +0,0 @@
-datapath=data/PublicAbdominalData
-dataname=01_Multi-Atlas_Labeling
-savepath=data/outs
-python -W ignore generate_datalist.py --data_path $datapath --dataset_name $dataname --out ./dataset/dataset_list --save_file $dataname.txt
